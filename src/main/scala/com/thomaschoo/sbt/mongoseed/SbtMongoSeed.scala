e555094a4cf8bd11dd46b52b7a05684a06fc8011
Thomas Choo <c.thomas.r@gmail.com>
2015-01-12 21:29:18 -0800

    Refactor to support different env seeding

--- src/main/scala/com/thomaschoo/sbt/mongoseed/SbtMongoSeed.scala
+++ src/main/scala/com/thomaschoo/sbt/mongoseed/SbtMongoSeed.scala
@@ -23,9 +23,9 @@ object SbtMongoSeed extends AutoPlugin {
 
     mongoSeedUri <<= getMongoUri,
 
-    mongoSeedDev <<= seedDb,
-    mongoSeedTest <<= seedDb,
-    mongoSeedProd <<= seedDb
+    mongoSeedDev <<= seedDb("dev"),
+    mongoSeedTest <<= seedDb("test"),
+    mongoSeedProd <<= seedDb("prod")
   )
 
   def getMongoUri = Def.setting {
@@ -35,14 +35,27 @@ object SbtMongoSeed extends AutoPlugin {
     conf.getString("uri")
   }
 
-  def seedDb = Def.task {
+  def seedDb(env: String) = Def.task {
     import com.mongodb.BasicDBObject
-    import com.mongodb.util._
-    import com.typesafe.config.{ ConfigFactory, ConfigRenderOptions }
-    import scala.collection.JavaConversions._
+    import com.typesafe.config.Config
+
+    def seedDb(): Unit = {
+      import com.typesafe.config.ConfigFactory
+      import scala.collection.JavaConversions._
+
+      val db = connect(getMongoUri.value)
+      getFiles foreach { f =>
+        val coll = db(f.getName.split('.').head)
+        val entries = ConfigFactory.parseFile(f)
+        entries.getConfigList("inserts") foreach { x =>
+          coll.insert(getDbObject(x))
+        }
+      }
+    }
 
     def connect(uri: String): MongoDB = {
       import com.mongodb.casbah.Imports._
+
       val mongoUri = MongoClientURI(uri)
       val mongoClient = MongoClient(mongoUri)
       (mongoUri.database map { dbName =>
@@ -50,16 +63,16 @@ object SbtMongoSeed extends AutoPlugin {
       }).get
     }
 
-    val db = connect(mongoSeedUri.value)
-    val seedFiles = (mongoSeedDir.value / "dev" * "*.conf").get
-    seedFiles foreach { f =>
-      val coll = db(f.getName.split('.')(0))
-      val entries = ConfigFactory.parseFile(f)
-      entries.getConfigList("inserts") foreach { x =>
-        val json = x.root().render(ConfigRenderOptions.concise())
-        val obj = JSON.parse(json).asInstanceOf[BasicDBObject]
-        coll.insert(obj)
-      }
+    def getFiles: Seq[File] = (mongoSeedDir.value / env * "*.conf").get
+
+    def getDbObject(conf: Config): BasicDBObject = {
+      import com.mongodb.util._
+      import com.typesafe.config.ConfigRenderOptions
+
+      val json = conf.root().render(ConfigRenderOptions.concise())
+      JSON.parse(json).asInstanceOf[BasicDBObject]
     }
+
+    seedDb()
   }
 }
