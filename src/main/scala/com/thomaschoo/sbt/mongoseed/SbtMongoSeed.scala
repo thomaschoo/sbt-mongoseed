10a1faf523f78627c345f8afd1158a3317bd4e05
Thomas Choo <c.thomas.r@gmail.com>
2015-01-14 22:22:27 -0800

    Refactor to be more scala idiomatic

--- src/main/scala/com/thomaschoo/sbt/mongoseed/SbtMongoSeed.scala
+++ src/main/scala/com/thomaschoo/sbt/mongoseed/SbtMongoSeed.scala
@@ -6,8 +6,10 @@ import Keys._
 object SbtMongoSeed extends AutoPlugin {
 
   object autoImport {
-    val mongoSeedDir = settingKey[File]("The directory for the seed files.")
     val mongoSeedUri = settingKey[String]("The MongoDB connection string.")
+    val mongoSeedDirDev = settingKey[PathFinder]("The directory for the development seed files.")
+    val mongoSeedDirTest = settingKey[PathFinder]("The directory for the test seed files.")
+    val mongoSeedDirProd = settingKey[PathFinder]("The directory for the production seed files.")
     val mongoSeedDev = taskKey[Unit]("Seeds mongodb for development")
     val mongoSeedTest = taskKey[Unit]("Seeds mongodb for test")
     val mongoSeedProd = taskKey[Unit]("Seeds mongodb for production")
@@ -18,23 +20,26 @@ object SbtMongoSeed extends AutoPlugin {
   override def requires = sbt.plugins.JvmPlugin
 
   override def projectSettings: Seq[Setting[_]] = Seq(
-    mongoSeedDir := (baseDirectory.value / "conf" / "seeds"),
-
     mongoSeedUri <<= getMongoUri,
+    mongoSeedDirDev := (baseDirectory.value / "conf" / "seeds" / "dev"),
+    mongoSeedDirTest := (baseDirectory.value / "conf" / "seeds" / "test"),
+    mongoSeedDirProd := (baseDirectory.value / "conf" / "seeds" / "prod"),
 
-    mongoSeedDev <<= seedDb("dev"),
-    mongoSeedTest <<= seedDb("test"),
-    mongoSeedProd <<= seedDb("prod")
+    mongoSeedDev <<= seedDb(mongoSeedDirDev),
+    mongoSeedTest <<= seedDb(mongoSeedDirTest),
+    mongoSeedProd <<= seedDb(mongoSeedDirProd)
   )
 
   def getMongoUri = Def.setting {
     import com.typesafe.config.ConfigFactory
-    val file = baseDirectory.value / "conf" / "application.conf"
-    val conf = ConfigFactory.parseFile(file).getConfig("mongodb").resolve()
-    conf.getString("uri")
+    ConfigFactory
+      .parseFile(baseDirectory.value / "conf" / "application.conf")
+      .getConfig("mongodb")
+      .resolve()
+      .getString("uri")
   }
 
-  def seedDb(env: String) = Def.task {
+  def seedDb(path: SettingKey[PathFinder]) = Def.task {
     import com.mongodb.BasicDBObject
     import com.mongodb.casbah.MongoDB
     import com.typesafe.config.Config
@@ -43,8 +48,8 @@ object SbtMongoSeed extends AutoPlugin {
       import com.typesafe.config.ConfigFactory
       import scala.collection.JavaConversions._
 
-      val db = connect(getMongoUri.value)
-      getFiles foreach { f =>
+      val db = connect()
+      (path.value * "*.conf").get foreach { f =>
         val coll = db(f.getName.split('.').head)
         val entries = ConfigFactory.parseFile(f)
         entries.entrySet().toList sortBy (_.getKey) foreach { entry =>
@@ -60,17 +65,19 @@ object SbtMongoSeed extends AutoPlugin {
       }
     }
 
-    def connect(uri: String): MongoDB = {
+    def connect(): MongoDB = {
       import com.mongodb.casbah.Imports._
 
-      val mongoUri = MongoClientURI(uri)
+      val mongoUri = MongoClientURI(mongoSeedUri.value)
       val mongoClient = MongoClient(mongoUri)
-      (mongoUri.database map { dbName =>
-        mongoClient(dbName)
-      }).get
-    }
 
-    def getFiles: Seq[File] = (mongoSeedDir.value / env * "*.conf").get
+      mongoUri.database map { dbName =>
+        mongoClient(dbName)
+      } match {
+        case Some(db) => db
+        case None => throw new RuntimeException(s"Failed to connect to MongoDB with uri: ${mongoSeedUri.value}")
+      }
+    }
 
     def getDbObject(conf: Config): BasicDBObject = {
       import com.mongodb.util._
